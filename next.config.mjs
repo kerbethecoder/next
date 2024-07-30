69d16c41aa42574f74e81d16a4b0f6aba8e3ffc4
kerbethecoder <krby.cnts@gmail.com>
2024-07-30 23:37:54 +0800

    unoptimize images

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,9 @@
 const nextConfig = {
   basePath: process.env.NODE_ENV === 'production' ? '/sveltekit' : '',
   output: 'export', // <=== enables static exports
+  images: {
+    unoptimized: true,
+  },
   reactStrictMode: true,
 };
 
